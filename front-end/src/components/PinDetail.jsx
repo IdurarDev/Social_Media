e85a647f97bcb859aff4be0f615a7400ba62e186
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-30 18:52:10 +0100

    add button after image and create const for fetch comment by key in back-end sanity

--- front-end/src/components/PinDetail.jsx
+++ front-end/src/components/PinDetail.jsx
@@ -16,6 +16,19 @@ const PinDetail = ({ user }) => {
   const [addingComment, setAddingComment] = useState(false);
   const { pinId } = useParams();
 
+  const addComment = () => {
+    if(comment) {
+      setAddingComment(true);
+
+      client
+        .patch(pinId)
+        .setIfMissing({ comments: []})
+        .insert('after', 'comment[-1', [{ 
+          comment, 
+          _key: uuidv4(),
+        }])
+    }
+  }
   const fetchPinDetails = () => {
     let query = pinDetailQuery(pinId);
 
@@ -121,6 +134,13 @@ const PinDetail = ({ user }) => {
             onChange={(e) => setComment(e.target.value)}
             className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300"
           />
+          <button
+            type="button"
+            className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none"
+            onClick={addComment}
+          >
+            {addingComment ? 'Posting th comment...' : 'Posted!'}
+          </button>
         </div>
       </div>
     </div>
