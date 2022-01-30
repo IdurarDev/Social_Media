37e06f6cb528ca12efc164ad021a80f7c4bc310e
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-30 17:28:35 +0100

    add div after image in comment section and p tag inside this the username who comment

--- front-end/src/components/PinDetail.jsx
+++ front-end/src/components/PinDetail.jsx
@@ -94,6 +94,9 @@ const PinDetail = ({ user }) => {
                 alt="user-profile"
                 className="w-10 h-10 rounded-full cursor-pointer"  
                />
+               <div className="flex flex-col">
+                 <p className="font-bold">{comment.postedBy.userName}</p>
+               </div>
             </div>
           ))}
         </div>
