22db4b64b9939ce663044bf84986fed09638a681
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-30 15:29:48 +0100

    add two a tags and inside this a link to destination pinDetail

--- front-end/src/components/PinDetail.jsx
+++ front-end/src/components/PinDetail.jsx
@@ -45,8 +45,26 @@ const PinDetail = ({ user }) => {
       <div className="flex justify-center items-center md:items-start flex-initial">
         <img 
           src={pinDetail?.image && urlFor(pinDetail.image).url()} 
-          alt="" 
+          className="rounded-t-3xl rounded-b-lg" 
+          alt="user-post"
         />
+      </div>
+      <div className="w-full p-5 flex-1 xl:min-w-620">
+        <div className="flex items-center justify-between">
+          <div className="flex gap-2 items-center">
+            <a 
+              href={ `${pinDetail.image?.asset?.url}?dl=` } 
+              download
+              onClick={ (e) => e.stopPropagation() }
+              className='bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none'
+            >
+              <MdDownloadForOffline />
+            </a>
+          </div>
+          <a href={pinDetail.destination} target="_blank" rel="noreferrer">
+             {pinDetail.destination}
+          </a>
+        </div>
       </div>
     </div>
   );
