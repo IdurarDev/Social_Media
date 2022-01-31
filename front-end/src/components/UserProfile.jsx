cc7689e4f855688c8e86d5ea23cb878fc6606939
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-31 10:10:20 +0100

    add all import in UserProfile

--- front-end/src/components/UserProfile.jsx
+++ front-end/src/components/UserProfile.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AiOutlineLagout } from 'react-icons/ia'; 
+import { useParams, useNavigate } from 'react-router-dom';
+import { GoogleLogout } from 'react-google-login';
+import { userCreatedPinsQuery, userQuery, userSaved} from '../utils/data';
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
 
 const UserProfile = () => {
   return (
     <div>
       UserProfile
     </div>
-  );
+  ); 
 };
 
 export default UserProfile;
